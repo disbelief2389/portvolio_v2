e2e8df9d4939e0bdd93946be5932a66e35ce553e
v3i1ix <v3i1ix@proton.me>
2025-02-25 08:52:50 +1100

    Navbar styling coming along

--- js/load-common.js
+++ js/load-common.js
@@ -18,6 +18,30 @@ function loadHTML(elementId, filePath) {
 
 document.addEventListener('DOMContentLoaded', () => {
     loadHTML('navbar', '/navbar.html')
-        // .then(() => initializeSidebar());
+        .then(navbarElement => {
+            const homeLi = document.getElementById('home-li-a');
+            const aboutLi = document.getElementById('about-li-a');
+            const workLi = document.getElementById('work-li-a');
+            const contactLi = document.getElementById('contact-li-a');
+
+            let liArr = [homeLi, aboutLi, workLi, contactLi];
+
+            function addClassToNavItem() {
+                const currentURL = window.location.href;
+
+                if (currentURL.includes('about.html')) {
+                    aboutLi.classList.add('active-link');
+                } else if (currentURL.includes('index.html')) {
+                    homeLi.classList.add('active-link');
+                } else if (currentURL.includes('work.html')) {
+                    workLi.classList.add('active-link');
+                } else if (currentURL.includes('contact.html')) {
+                    contactLi.classList.add('active-link');
+                }
+            };
+
+// Call the function to add the class to the corresponding nav item
+addClassToNavItem();
+        });
     loadHTML('footer', '/footer.html');
 });
