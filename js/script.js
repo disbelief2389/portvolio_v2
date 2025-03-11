f65678e3a51bcda942cd53e5680130f9ee972258
v3i1ix <v3i1ix@proton.me>
2025-03-12 09:21:37 +1100

    Change formatting

--- js/script.js
+++ js/script.js
@@ -1,72 +1,68 @@
 // Sidebar functionality
 function initializeSidebar() {
-    const navbar = document.getElementById('navbar-nav');
-    const navbarDiv = document.getElementById('navbar');
-    const openBtn = document.getElementById('open-sidebar-button');
-    const closeBtn = document.getElementById('close-sidebar-button');
-    const html = document.documentElement;
-    // const audioControls = document.getElementById('audio-controls');
-    // const overlay = document.getElementById('overlay');
-  
-  
-    function updateButtonStates () {
-      if (navbar.classList.contains('show')) {
-        
-        html.style.overflow = 'hidden';
+  const navbar = document.getElementById("navbar-nav");
+  const navbarDiv = document.getElementById("navbar");
+  const openBtn = document.getElementById("open-sidebar-button");
+  const closeBtn = document.getElementById("close-sidebar-button");
+  const html = document.documentElement;
+  // const audioControls = document.getElementById('audio-controls');
+  // const overlay = document.getElementById('overlay');
 
-        // when sidebar is open:
-        openBtn.style.opacity = 0;
-        openBtn.style.pointerEvents = 'none';
-        closeBtn.style.opacity = 1;
-        closeBtn.style.pointerEvents = 'auto';
-  
-        // if (audioControls) {
-        //   audioControls.style.opacity = 0;
-        //   setTimeout(() => {
-        //     audioControls.classList.add('hidden');
-        //   }, 300);
-        // }
-        overlay.style.display = 'block';
-        overlay.style.pointerEvents = 'auto';
-        setTimeout(() => {
-            overlay.style.opacity = 1;
-        }, 50);
-  
-      } else {
+  function updateButtonStates() {
+    if (navbar.classList.contains("show")) {
+      html.style.overflow = "hidden";
 
-        html.style.overflow = 'visible';
-  
-        // when sidebar is closed:
-        openBtn.style.opacity = 1;
-        openBtn.style.pointerEvents = 'auto';
-        closeBtn.style.opacity = 0;
-        closeBtn.style.pointerEvents = 'none';
-  
-        // if (audioControls) {
-        //   audioControls.style.opacity = 1;
-        //   setTimeout(() => {
-        //     audioControls.classList.remove('hidden');
-        //   }, 300);
-        // }
-        overlay.style.opacity = 0;
-        overlay.style.pointerEvents = 'none';
-        setTimeout(() => {
-            overlay.style.display = 'none';
-        }, 300)
-      }
+      // when sidebar is open:
+      openBtn.style.opacity = 0;
+      openBtn.style.pointerEvents = "none";
+      closeBtn.style.opacity = 1;
+      closeBtn.style.pointerEvents = "auto";
+
+      // if (audioControls) {
+      //   audioControls.style.opacity = 0;
+      //   setTimeout(() => {
+      //     audioControls.classList.add('hidden');
+      //   }, 300);
+      // }
+      overlay.style.display = "block";
+      overlay.style.pointerEvents = "auto";
+      setTimeout(() => {
+        overlay.style.opacity = 1;
+      }, 50);
+    } else {
+      html.style.overflow = "visible";
+
+      // when sidebar is closed:
+      openBtn.style.opacity = 1;
+      openBtn.style.pointerEvents = "auto";
+      closeBtn.style.opacity = 0;
+      closeBtn.style.pointerEvents = "none";
+
+      // if (audioControls) {
+      //   audioControls.style.opacity = 1;
+      //   setTimeout(() => {
+      //     audioControls.classList.remove('hidden');
+      //   }, 300);
+      // }
+      overlay.style.opacity = 0;
+      overlay.style.pointerEvents = "none";
+      setTimeout(() => {
+        overlay.style.display = "none";
+      }, 300);
     }
-  
-    function openSidebar() {
-        navbar.classList.add('show');
-        updateButtonStates();
-    }
-  
-    function closeSidebar() {
-        navbar.classList.remove('show');
-        updateButtonStates();
-    }
-  
-    openBtn.addEventListener('click', openSidebar);
-    closeBtn.addEventListener('click', closeSidebar);
-    overlay.addEventListener('click', closeSidebar);
+  }
+
+  function openSidebar() {
+    navbar.classList.add("show");
+    updateButtonStates();
+  }
+
+  function closeSidebar() {
+    navbar.classList.remove("show");
+    updateButtonStates();
+  }
+
+  openBtn.addEventListener("click", openSidebar);
+  closeBtn.addEventListener("click", closeSidebar);
+  overlay.addEventListener("click", closeSidebar);
 }
